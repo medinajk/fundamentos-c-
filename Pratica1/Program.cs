2a49b63b2087c6f8a145e0080bae3ba00e68486a
Bianca Medina <bmmedina.dev@gmail.com>
2026-04-14 18:54:18 -0300

    Initial commit

---
+++ Pratica1/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+public class Program
+{
+   public static void Main()
+    {
+        Console.WriteLine("Digite seu nome:");
+        string nome = Console.ReadLine();
+        Console.WriteLine("Olá, " + nome + "!");
+    }
+}
